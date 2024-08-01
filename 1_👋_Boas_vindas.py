bd0376572ab24375d661671daca05c51d84cdeab
wmoural <68879544+wmoural@users.noreply.github.com>
2024-07-31 22:20:49 -0300

    Update 1_👋_Boas_vindas.py

--- 1_👋_Boas_vindas.py
+++ 1_👋_Boas_vindas.py
@@ -10,7 +10,7 @@ with st.sidebar:
             sidebarcol1, sidebarcol2 = st.columns([2, 1])
             with sidebarcol1:
                 imagem = 'https://i.imgur.com/TmV4rOH.jpg'
-                st.image(imagem, use_column_width=True, caption='Wellington Moura')
+                st.image(imagem, use_column_width=False, caption='Wellington Moura')
     
             with sidebarcol2:
                 st.header('')
