06c6088f46c02d7f3247a4ff3791904a5bc8bdd4
wmoural <68879544+wmoural@users.noreply.github.com>
2024-10-04 15:46:08 -0300

    Update 4_🛣️_OvertureMaps.py

--- pages/4_🛣️_OvertureMaps.py
+++ pages/4_🛣️_OvertureMaps.py
@@ -150,27 +150,6 @@ with col2:
                         
                     status.update(label=f'**:green[Filtragem completa: {len(st.session_state.resultado)} resultados encontrados!]** :partying_face:', state='complete', expanded=False)
                                        
-        if st.session_state.resultado is not None:
-            st.dataframe(st.session_state.resultado)
-            
-            # Gerando gdf de resultado no cache
-            #buffer = io.BytesIO()
-            
-            #with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-                #st.session_state.resultado.to_excel(writer, sheet_name='Sheet1')
-                #writer.close()    
-
-            # Criando botão de download
-            #cl1,cl2,cl3 = st.columns([2,2,2])
-            
-            #with cl2:
-
-                #st.download_button(
-                    #label="**:green[Download em excel (WKT)] ✅**",
-                    #data=buffer,
-                    #file_name=f"base-{busca}.xlsx",
-                    #mime="application/vnd.ms-excel",
-                    #use_container_width=True)
             
 with col1:
     
@@ -189,6 +168,6 @@ with col1:
 # Mostrando tabela com resultados finais
 if st.session_state.resultado is not None and arquivo is not None:
     
-    with st.expander('**Prévia dos resultados:** :page_with_curl:'):
+    with st.expander('**Resultados:** ✔️'):
         
-        st.session_state.resultado[:101]
+        st.dataframe(st.session_state.resultado)
