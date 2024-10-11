186880af2372322aad57c3ac4d274e5d8f642472
wmoural <68879544+wmoural@users.noreply.github.com>
2024-10-11 18:22:42 -0300

    dsaas

--- 1_👋_Boas_vindas.py
+++ 1_👋_Boas_vindas.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configurando página
 st.set_page_config(page_title='Easy GeoMax!')
-st.logo('https://i.imgur.com/Pii52l6.png', icon_image='https://i.imgur.com/u69mszZ.png')
+st.logo('https://i.imgur.com/eIPSxlZ.png', icon_image='https://i.imgur.com/u69mszZ.png')
 
     
 with st.sidebar:
