f62fcebc8a3081bf2030565a7a789c98f2cc02a0
wmoural <68879544+wmoural@users.noreply.github.com>
2025-04-17 15:14:10 -0300

    Update 6_🚏_Clusterization_and_Routing.py

--- pages/6_🚏_Clusterization_and_Routing.py
+++ pages/6_🚏_Clusterization_and_Routing.py
@@ -289,7 +289,7 @@ if 'buffer_rotas' not in st.session_state:
     st.session_state.buffer_rotas = None
 
 # Configurando página
-st.set_page_config(page_title="Easy OvertureData", page_icon=':material/tactic:', layout='wide')
+st.set_page_config(page_title="Easy Clusterization and Routing", page_icon=':material/tactic:', layout='wide')
 
 # Ajustando CSSs
 st.markdown("""
@@ -308,7 +308,7 @@ st.markdown("""
             """, unsafe_allow_html=True)
 
 # Título
-st.title("**Easy** :blue[Routing] :busstop:")   
+st.title("**Easy** :orange[Routing] :busstop:")   
 
 # Criando tabs
 tab1, tab2 = st.tabs(['Clusterização', 'Roteirização'])
