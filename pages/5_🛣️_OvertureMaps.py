9048ad52e7082a613ad6f0bf9a0e214a760269f2
wmoural <68879544+wmoural@users.noreply.github.com>
2025-04-17 15:38:35 -0300

    Update 5_🛣️_OvertureMaps.py

--- pages/5_🛣️_OvertureMaps.py
+++ pages/5_🛣️_OvertureMaps.py
@@ -60,7 +60,7 @@ with st.sidebar:
             sidebarcol1, sidebarcol2 = st.columns([2, 1])
             with sidebarcol1:
                 imagem = 'https://i.imgur.com/Xe9O2MX.png'
-                st.image(imagem, use_column_width=True, caption='Wellington Moura')
+                st.image(imagem, use_container_width=True, caption='Wellington Moura')
     
             with sidebarcol2:
                 st.header('')
@@ -69,7 +69,7 @@ with st.sidebar:
     
     with st.expander('**Pague-me um café:**', icon='☕', expanded=False):
         pix = 'https://i.imgur.com/LLr5WY8.jpg'
-        st.image(pix, use_column_width='True', caption='PIX')
+        st.image(pix, use_container_width='True', caption='PIX')
     
 # Definindo uploader de arquivo    
 arquivo = st.file_uploader('**Faça o upload da área de interesse aqui** :call_me_hand:', type=['.gpkg'])
