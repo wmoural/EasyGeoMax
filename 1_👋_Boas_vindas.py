d45298bb57e66e7581abc2bfb6b259dda3cd0f69
wmoural <68879544+wmoural@users.noreply.github.com>
2025-09-15 22:18:55 -0300

    Update 1_👋_Boas_vindas.py

--- 1_👋_Boas_vindas.py
+++ 1_👋_Boas_vindas.py
@@ -6,36 +6,37 @@ st.set_page_config(page_title='Easy GeoMax!', layout='wide', page_icon=':materia
 # Funções
 @st.cache_data
 def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não importa)
-    if arquivo is None:
-        with st.container(horizontal_alignment='center'):
-            
-            st.title('Easy :green[GeoMax! :material/distance:]', width='content')
-            for i in range(3):st.text('')
-            st.subheader(':gray[:material/help: Uso]', width='content')    
-            col1,col2,col3 = st.columns([0.2,.6,0.2])
+    with st.container(horizontal_alignment='center'):
         
-            with col2:
-                with st.container(horizontal_alignment='left'):
-                    st.markdown(':gray[:material/info:] Site em manutenção, porém uso ainda liberado.', width='content')
-
-            
-            # Ajustes de CSS
-            st.markdown("""
-                        <style>
+        st.title('Easy :green[GeoMax! :material/distance:]', width='content')
+        for i in range(3):st.text('')
+        st.subheader(':gray[:material/help: Uso]', width='content')    
+        col1,col2,col3 = st.columns([0.2,.6,0.2])
     
-                        .st-emotion-cache-1fc0ges p {
-                            margin-top: -19px;
-                            }
-                        
-                        .st-emotion-cache-10p9htt {
-                            height: 1rem;
-                            margin-bottom: 10px;                            
-                            }
-                        
-                        .st-emotion-cache-1s2v671 {
-                            min-height: 0rem;
+        with col2:
+            with st.container(horizontal_alignment='left'):
+                st.markdown(':gray[:material/info:] Site em manutenção, porém uso ainda liberado.', width='content')
+
+        
+        # Ajustes de CSS
+        st.markdown("""
+                    <style>
+
+                    .st-emotion-cache-1fc0ges p {
+                        margin-top: -19px;
                         }
-                        
-                        </style>
-                    """,
-                    unsafe_allow_html=True)
+                    
+                    .st-emotion-cache-10p9htt {
+                        height: 1rem;
+                        margin-bottom: 10px;                            
+                        }
+                    
+                    .st-emotion-cache-1s2v671 {
+                        min-height: 0rem;
+                    }
+                    
+                    </style>
+                """,
+                unsafe_allow_html=True)
+
+carregar_layout()
