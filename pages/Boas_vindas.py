0fa523d71f68767ddd466f1c31b8714e6ef4b649
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-08 10:11:53 -0300

    Update Boas_vindas.py

--- pages/Boas_vindas.py
+++ pages/Boas_vindas.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 # Configurando página
-st.set_page_config(page_title='Easy GeoMax!', layout='wide', page_icon=':material/manufacturing:')
+st.set_page_config(page_title='Easy GeoMax!', layout='wide', page_icon=':material/home:')
 
 st.title('Easy :green[GeoMax! :material/distance:]', width='content')
 st.caption('Site em manutenção...')
+
