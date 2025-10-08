0bc8450ff0d717566661acf414ac8dc8546f8854
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-08 16:55:00 -0300

    Update Boas_vindas.py

--- paginas/Boas_vindas.py
+++ paginas/Boas_vindas.py
@@ -7,12 +7,8 @@ st.set_page_config(page_title='Easy GeoMax!', layout='wide', page_icon=':materia
 with st.container(horizontal_alignment='center'):
     st.title('Easy :green[GeoMax!]', width='content')
     st.caption('Aplicação web multifuncionalidade', width='content') 
-"""
-with st.container(horizontal_aligment='center'):
+
+with st.container(horizontal_alignment='center'):
     col1,col2,col3,col4 = st.columns([0.25, 0.25, 0.25, 0.25])
     with col1:
         'st.header('teste')
-"""
-
-
-
