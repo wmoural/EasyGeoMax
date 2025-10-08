d235b2ee7f332b68b8e4f40852ea86cb09169a70
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-08 17:00:13 -0300

    Update Boas_vindas.py

--- paginas/Boas_vindas.py
+++ paginas/Boas_vindas.py
@@ -9,15 +9,16 @@ with st.container(horizontal_alignment='center'):
     st.caption('Aplicação web multifuncionalidade', width='content') 
 
 with st.container(horizontal_alignment='center'):
+    col1,col2,col3,col4 = st.columns(4, border=True)
     
-    with st.columns(border=True, width='stretch'):
+    with col1:
         st.header('teste')
         
-    with st.columns(border=True, width='stretch'):
+    with col2:
         st.header('teste')
-        
-    with st.columns(border=True, width='stretch'):
+
+    with col3:
         st.header('teste')
-        
-    with st.columns(border=True, width='stretch'):
+
+    with col4:
         st.header('teste')
