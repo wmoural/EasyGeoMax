737685ba205145ac331c6629a651b832c392aa80
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-08 16:40:15 -0300

    Update Boas_vindas.py

--- paginas/Boas_vindas.py
+++ paginas/Boas_vindas.py
@@ -3,38 +3,7 @@ import streamlit as st
 # Configurando página
 st.set_page_config(page_title='Easy GeoMax!', layout='wide', page_icon=':material/home:')
 
-
-st.caption('Site em manutenção...')
-
-
-@st.cache_data
-def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não importa)
-    if arquivo is None:
-        with st.container(horizontal_alignment='center'):
-            
-            st.title('Easy :green[GeoMax!]', width='content')
-            st.caption('Aplicação web multifuncionalidade', width=370) 
-            
-            # Ajustes de CSS
-            st.markdown("""
-                        <style>
-    
-                        .st-emotion-cache-1fc0ges p {
-                            margin-top: -19px;
-                            }
-                        
-                        .st-emotion-cache-10p9htt {
-                            height: 1rem;
-                            margin-bottom: 10px;                            
-                            }
-                        
-                        .st-emotion-cache-1s2v671 {
-                            min-height: 0rem;
-                        }
-                        
-                        </style>
-                    """,
-                    unsafe_allow_html=True)
-
-carregar_layout()
-
+# Carregando layout
+with st.container(horizontal_alignment='center'):
+    st.title('Easy :green[GeoMax!]', width='content')
+    st.caption('Aplicação web multifuncionalidade', width=370) 
