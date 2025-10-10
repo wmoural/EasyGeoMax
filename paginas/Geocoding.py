67a689ed6cb80571f654a7dbef889e2e414cb925
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-10 16:59:45 -0300

    Update Geocoding.py

--- paginas/Geocoding.py
+++ paginas/Geocoding.py
@@ -30,7 +30,7 @@ def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não
                     st.markdown(':gray[:material/counter_2:] Forneça uma chave API caso queira geocodificar com Google (opção paga) ou deixe em branco para geocodificar com ArcGIS (opção gratuita)', width='content')
                     st.markdown(':gray[:material/counter_3:] Defina a coluna em que estão os endereços e geocodifique', width='content')
                     st.markdown(':gray[:material/counter_4:] Visualize e baixe os resultados', width='content')
-            
+            """
             # Ajustes de CSS
             st.markdown("""
                         <style>
@@ -51,13 +51,13 @@ def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não
                         </style>
                     """,
                     unsafe_allow_html=True)
-     
+             """
     else:
         with st.container(horizontal_alignment='center'):
             st.title('Easy :green[Geocoding :material/globe_location_pin:]', width='content')
             st.caption('Aplicação web para realização de geocoding de endereços', width=370)
             st.divider()
-            
+            """
             # Ajustes de CSS
             st.markdown("""
                         <style>
@@ -89,7 +89,7 @@ def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não
                         </style>
                     """,
                     unsafe_allow_html=True)
-
+            """
 def Geocodificar(df = pd.DataFrame, Chave = str) -> pd.DataFrame():
     
     # Reduzindo tamando do dataframe
