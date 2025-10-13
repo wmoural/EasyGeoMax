e77968ca7adc20986b0b7c0eb0580d6c54d58a7d
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-13 17:16:43 -0300

    Update Boas_vindas.py

--- paginas/Boas_vindas.py
+++ paginas/Boas_vindas.py
@@ -11,20 +11,21 @@ with st.container(horizontal_alignment='center'):
 col1,col2,col3,col4 = st.columns(4, border=True, vertical_alignment ='top')
 
 with col1:
-    col1.container(height=500, border=False)
+    col1.container(height=500, border=False, vertical_alignment ='top')
     st.title('Easy :green[Geocoding :material/globe_location_pin:]', width='content')
     
 with col2:
-    col2.container(height=500, border=False)
+    col2.container(height=500, border=False, vertical_alignment ='top')
     st.title('Easy :blue[Reverse Geocoding :material/travel_explore:]', width='content')
 
 with col3:
-    col3.container(height=500, border=False)
+    col3.container(height=500, border=False, vertical_alignment ='top')
     st.title('Easy :violet[Routes :material/route:]', width='content')
 
 with col4:
-    col4.container(height=500, border=False)
+    col4.container(height=500, border=False, vertical_alignment ='top')
     st.title('Easy :red[Overture :material/south_america:]', width='content')
+
 
 
 
