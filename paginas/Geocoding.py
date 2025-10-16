914a7b8c570c2d61fd505d066c20425e5f4a6b02
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-16 19:45:02 -0300

    Update Geocoding.py

--- paginas/Geocoding.py
+++ paginas/Geocoding.py
@@ -39,7 +39,6 @@ def carregar_layout(): # Função para ajustar o layout (coisa de frontend, não
         with st.container(horizontal_alignment='center'):
             st.title('Easy :green[Geocoding :material/globe_location_pin:]', width='content')
             st.caption('Aplicação web para realização de geocoding de endereços', width=370)
-            st.divider()
             
         # Ajustes de CSS
         st.markdown(uploader_depois(arquivo.name), unsafe_allow_html=True)
