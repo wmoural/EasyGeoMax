27780743ef6f402b69fb093e4dbc0f8d1d85a2fe
wmoural <68879544+wmoural@users.noreply.github.com>
2025-10-16 20:21:52 -0300

    Update Geocoding.py

--- paginas/Geocoding.py
+++ paginas/Geocoding.py
@@ -140,7 +140,10 @@ if 'Resultado' not in st.session_state:
     st.session_state.Resultado = None
 
 # Sidebar
-with st.sidebar: 
+with st.sidebar:
+    # Logo
+    with st.container(horizontal_alignment='center'):   
+    st.header(':green[EasyGeoMax]', width='content')
            
     # Botão para subir planilha excel
     arquivo = st.file_uploader(':blue[:material/upload_file: Faça o upload da planilha excel]', type=['xlsx'])
